e270a1bbf58a2e8e9d13b9892d6f54566f7193bf
Snir <snir@aporia.com>
2020-08-14 19:36:32 +0300

    Fixes the contact form

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -166,7 +166,7 @@
 										נושא פנייה
 									</label>
 									<select
-										v-model="contactFormData.subject"
+										class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none focus:border-blue-700 mb-2 p-4"
 										required
 										name="subject"
 										autocomplete="off"
