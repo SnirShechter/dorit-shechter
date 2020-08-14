5c917264e171b6b726a47aaa8df03f4571f628c6
Snir <snir@aporia.com>
2020-08-14 22:35:01 +0300

    Fixes the contact form

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -162,7 +162,7 @@
 							</div>
 							<div class="flex flex-wrap mb-6 -mx-4">
 								<div class="w-full px-4 md:w-1/2">
-									<label class="block text-copy-primary mb-2">
+									<label class="block text-copy-primary mb-2" for="subject">
 										נושא פנייה
 									</label>
 									<select
@@ -170,6 +170,7 @@
 										@input="contactFormData.subject = $event.target.value"
 										class="block w-full bg-background-form border border-border-color-primary shadow rounded outline-none focus:border-blue-700 mb-2 p-4"
 										required
+										id="subject"
 										name="subject"
 										autocomplete="off"
 									>
